0f7b3c6590b66585799d945b07cb21080176c923
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-18 19:49:49 +0400

    App starts

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import express from "express"
+import { config } from "dotenv"
+
+config()
+const app = express()
 
 
+
+const start = () => {
+    try {
+        app.listen(process.env.PORT, () => console.log("APP WAS STARTED"))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
