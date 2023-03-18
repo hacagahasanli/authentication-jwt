4a2c5bd874e9a6c8f51d33d40e4de22aa49f180c
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-18 21:00:57 +0400

    User Registration

--- controller/authController.js
+++ controller/authController.js
@@ -1,22 +1,27 @@
 import User from "../models/User.js";
 import Role from "../models/Role.js";
+import bcrypt from "bcryptjs"
 
 class AuthController {
-    async registration() {
+    async registration(req, res) {
         try {
             const { username, password } = req.body
             const candidate = await User.findOne({ username })
             if (candidate) {
                 return res.status(400).json({ message: "The user with this username already exists" })
             }
+            const hashPassword = bcrypt.hashSync(password, 7)
+            const userRole = await Role.findOne({ value: "USER" })
+            const user = new User({ username, password: hashPassword, roles: [userRole.value] })
+            await user.save()
+            return res.json({ message: "User successfully registrated" })
         } catch (err) {
             console.log(err);
             res.status(400).json({ messgae: "Registration Error" })
         }
     }
-    async login() {
+    async login(req, res) {
         try {
-
         } catch (err) {
             console.log(err);
             res.status(400).json({ messgae: "Login Error" })
